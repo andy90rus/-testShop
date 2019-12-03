fb2159aeec4bd22d18d68d8e9f9d824693c91a40
andrey.korobeynikov <andy90rus@gmail.com>
2019-12-03 17:57:02 +0300

    Added orderType

--- src/entities/interfaces/IOrder.interface.ts
+++ src/entities/interfaces/IOrder.interface.ts
@@ -1,10 +1,12 @@
 import { IStore } from './IStore.interface';
+import { OrderType } from '../enums/orderType.enum';
 
 export interface IOrder {
 	id: number;
 	created: Date;
 	baseAmount: number;
 	totalAmount: number;
+	orderType: OrderType;
 	storeId: number;
 	store: IStore;
 }
--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -3,6 +3,7 @@ import { IStore } from './interfaces/IStore.interface';
 import { IOrder } from './interfaces/IOrder.interface';
 import { StoreEntity } from './store.entity';
 import { IStoreDto } from '../dtos/interfaces/IStoreDto.interface';
+import { OrderType } from './enums/orderType.enum';
 
 @Entity({name: 'orders'})
 export class OrderEntity extends BaseEntity implements IOrder {
@@ -12,6 +13,8 @@ export class OrderEntity extends BaseEntity implements IOrder {
 	public created: Date;
 	@Column('decimal', {scale: 2, precision: 10, default: 0.0})
     public baseAmount: number;
+	@Column({type: 'enum', enum: OrderType, nullable: true})
+	public orderType: OrderType;
 	@Column('decimal', {scale: 2, precision: 10, default: 0.0})
     public totalAmount: number;
 	@RelationId((store: OrderEntity) => store.store)
